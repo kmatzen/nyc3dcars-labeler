8c9a7bb82872405d55562f6e0112284ab59e6849
Kevin James Matzen <kmatzen@cs.cornell.edu>
2013-10-30 22:25:05 -0400

    A smaller installer fix.

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 APP=labeler
-SERVER_NAME=TODO
+SERVER_NAME=localhost
 
 sudo apt-get install python-virtualenv nginx uwsgi uwsgi-plugin-python python-dev postgresql-server-dev-9.1 postgresql-9.1
 
@@ -15,13 +15,15 @@ sudo ln -s -f $PWD/conf/$SERVER_NAME.conf /etc/nginx/sites-enabled/
 sed -e "s;%SERVER_NAME%;$SERVER_NAME;" -e "s;%ROOT%;$PWD;" -e "s;%APP%;$APP;" conf/uwsgi.ini.template > conf/$SERVER_NAME.ini
 sudo ln -s -f $PWD/conf/$SERVER_NAME.ini /etc/uwsgi/apps-enabled/
 
+echo "Please set the default password to postgres in src/labeler/database.py.  The default is password123."
 sudo -u postgres psql postgres -c "\password postgres"
 sudo -u postgres createdb labeler
 
 sudo rm /etc/nginx/sites-enabled/default
 
-psql labeler -f schema.sql
+sudo -u postgres psql labeler -f schema.sql
 
 sudo service uwsgi restart
 sudo service nginx restart
 
+echo "Please create your site config file in src/labeler/instance/settings.cfg.  There is a template in that directory."
